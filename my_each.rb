88c0a39ecdfb1ecfe1f7be3cb95cf00906365c69
layla dantas <laylatdantas@gmail.com>
2019-05-22 22:23:12 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,12 @@
 def my_each(array)
   i = 0 
   
+ 
   
-  array.each do |word|
-    puts |word|
-    yeld 
-  
-  
+  collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
+end
   # code here
   
 end
